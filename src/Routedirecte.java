c555399920672cbf46a2e8a77ecc7b510eb2b34b
younss95 <younesazirgui@gmail.com>
2025-01-27 11:50:42 +0100

    ajout de la documentation de la classe Routedirecte

--- src/Routedirecte.java
+++ src/Routedirecte.java
@@ -1,4 +1,19 @@
+/**
+ * Classe Routedirecte qui implemente l'interface SuivreRoute
+ * et qui renvoie la route à suivre ( en l'occurence la route directe )
+ *
+ *
+ * @author Younes Azirgui
+ *
+ */
+
+
 public class Routedirecte implements SuivreRoute {
+
+    /**
+     * Affiche "Route directe" dans la console
+     *
+     */
 
     public void suivreRoute(){
         System.out.println("Route Directe");

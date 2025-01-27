0adaa0493443ccb59bce65298c5c5031483a0214
younss95 <younesazirgui@gmail.com>
2025-01-27 11:48:13 +0100

    documentation de la classe RouteAlizes

--- src/RouteAlizes.java
+++ src/RouteAlizes.java
@@ -1,5 +1,21 @@
+/**
+ * Classe Route Alizes qui implemente l'interface SuivreRoute
+ * et qui renvoie la route à suivre ( en l'occurence la route Alizes )
+ *
+ *
+ * @author Younes Azirgui
+ *
+ */
+
+
+
 public class RouteAlizes implements SuivreRoute {
 
+
+    /**
+     * Affiche "Route Alizes" dans la console
+     *
+     */
 
     public void suivreRoute(){
         System.out.println("Route Alizes");

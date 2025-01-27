999e5e75567beed35e1d6d8dc7ad5359059066a3
younss95 <younesazirgui@gmail.com>
2025-01-27 12:01:26 +0100

    documentation de l'interface SuivreRoute

--- src/SuivreRoute.java
+++ src/SuivreRoute.java
@@ -1,5 +1,18 @@
+/**
+ * Interface SuivreRoute qui declare la méthode suivreRoute
+ * qui sera implementée dans les classes qui heriteront de cette classe
+ *
+ * @author Younes Azirgui
+ *
+ */
+
+
 public interface SuivreRoute {
 
+
+    /**
+     * Méthode suivreRoute pour suivre une route spécifique
+     */
     public void suivreRoute();
 
 }

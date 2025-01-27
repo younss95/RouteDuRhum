5c0d1dc935d685b4e8dc226beb6a00cdc9e8b43c
younss95 <younesazirgui@gmail.com>
2025-01-27 12:08:12 +0100

    modification de la documentation de la classe Client

--- src/Client.java
+++ src/Client.java
@@ -7,9 +7,13 @@
  */
 
 public class Client {
+
+    /**
+     * Création de deux voiliers avec leur routes associées
+     */
     public static void main(String[] args) {
-        Monocoque mo = new Monocoque("le abdel", new RouteAlizes());
-        Multicoque mu = new Multicoque("le aziz", new Routedirecte());
+        Monocoque mo = new Monocoque("Voilier1", new RouteAlizes());
+        Multicoque mu = new Multicoque("Voilier2", new Routedirecte());
 
         System.out.println(mo);
     }
